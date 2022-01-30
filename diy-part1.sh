4a06113b1a86d994f154c71196e31f3cb3459d18
lilix9910 <98016077+lilix9910@users.noreply.github.com>
2022-01-31 00:19:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@
 
 echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
 echo 'src-git packages https://github.com/openwrt/packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
